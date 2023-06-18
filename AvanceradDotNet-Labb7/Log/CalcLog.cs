6da3c0c11b5df1ab4c381e8d78869af05d6c93a0
PetterBostrom <petter.bostrom@gmail.com>
2023-06-18 17:19:21 +0200

    New repo since I had problems with the last one.

---
+++ AvanceradDotNet-Labb7/Log/CalcLog.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvanceradDotNet_Labb7.Log
+{
+    public class CalcLog
+    {
+        public double First { get; set; }
+        public double Second { get; set; }
+        public double Result { get; set; }
+        public string CalculationMethod { get; set; }
+        public string TimeCreated { get; set; }
+        
+        // Constructor for use in app.
+        public CalcLog(double a, double b, double c, string calc)
+        {
+            First = a;
+            Second = b;
+            Result = c;
+            CalculationMethod = calc;
+            TimeCreated = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+        // Constructor for reading from file.
+        public CalcLog(double a, double b, double c, string calc, string time)
+        {
+            First = a;
+            Second = b;
+            Result = c;
+            CalculationMethod = calc;
+            TimeCreated = time;
+        }
+
+        public void PrintWithDate()
+        {
+            Console.Write(TimeCreated + " " + First);
+            switch (CalculationMethod)
+            {
+                case "Addition":
+                    Console.Write(" + ");
+                    break;
+                case "Subtraction":
+                    Console.Write(" - ");
+                    break;
+                case "Multiplication":
+                    Console.Write(" * ");
+                    break;
+                case "Division":
+                    Console.Write(" / ");
+                    break;
+            }
+            Console.Write(Second + " = " + Result + "\n");
+        }
+        public void Print()
+        {
+            Console.Write(First);
+            switch (CalculationMethod)
+            {
+                case "Addition":
+                    Console.Write(" + ");
+                    break;
+                case "Subtraction":
+                    Console.Write(" - ");
+                    break;
+                case "Multiplication":
+                    Console.Write(" * ");
+                    break;
+                case "Division":
+                    Console.Write(" / ");
+                    break;
+            }
+            Console.Write(Second + " = " + Result + "\n");
+        }
+    }
+}
---
+++ AvanceradDotNet-Labb7/Log/LogStack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AvanceradDotNet_Labb7.Log
+{
+    public static class LogStack
+    {
+        public static Stack<CalcLog> logs = new Stack<CalcLog>();
+
+        public static void AddToLog(CalcLog log)
+        {
+            logs.Push(log);
+        }
+
+        public static void PrintLog()
+        {
+            foreach (var log in logs)
+            {
+                log.PrintWithDate();
+            }
+        }
+
+        public static void WriteLogsToFile(string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                foreach (var log in logs)
+                {
+                    sw.WriteLine($"{log.First};{log.Second};" +
+                        $"{log.Result};{log.CalculationMethod};" +
+                        $"{log.TimeCreated}");
+                }
+            }
+        }
+        public static void GetLogsFromFile(string path)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string[] logs = sr.ReadToEnd().Split('\n');
+                foreach (string log in logs)
+                {
+                    // Check that string is not empty.
+                    if (log != "")
+                    {
+                        string[] logProps = log.Split(";");
+                        CalcLog temp = new CalcLog(Convert.ToDouble(logProps[0]),
+                            Convert.ToDouble(logProps[1]),
+                            Convert.ToDouble(logProps[2]),
+                            logProps[3], logProps[4].Trim('\r'));
+                        LogStack.AddToLog(temp);
+                    }
+                }
+            }
+        }
+    }
+}
